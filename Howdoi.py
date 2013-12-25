481c5806043afbbf8297f842c1392f5421933332
Santiago Agüero <aguerosantiale@gmail.com>
2013-12-25 20:10:30 -0300

    Adding Support for Windows for SublimeText 3

--- Howdoi.py
+++ Howdoi.py
@@ -5,19 +5,24 @@ class HowdoiDirectCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for region in self.view.sel():
                 line = self.view.line(region)
-                cont =self.view.substr(line)
+                cont = self.view.substr(line)
 
                 if (len(cont)<1):
-                	pass
+                    pass
                 else:        
-	                p = subprocess.Popen("/usr/local/bin/howdoi "+cont,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
-	                output, errors = p.communicate()
+                    p = subprocess.Popen("howdoi " + cont,
+                                         stdout=subprocess.PIPE,
+                                         stderr=subprocess.PIPE,
+                                         shell=True)
+                    output, errors = p.communicate()
 
-	                self.view.replace(edit, line, output)
+                    # Python 3 returns binary data, hence we have to decode it.
+                    # Python 2 won't be affected by this decoding.
+                    output = output.decode('ascii')
 
+                    # At least in Windows, output would contain carriage return
+                    # characters (CR). We have to remove them in order to look
+                    # properly.
+                    output = output.replace('\r', '')
 
-
-
-
-
-
+                    self.view.replace(edit, line, output)
